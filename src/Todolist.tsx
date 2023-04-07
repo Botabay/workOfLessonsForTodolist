42d8ed7280dc87a4d5d456e132b4a9d2c8543664
Botabay <tohgl49@gmail.com>
2023-04-07 11:18:05 +0600

    my deleting an element without deleting from array,by using HTML

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -12,6 +12,9 @@ type PropsType = {
 }
 
 export function Todolist(props: PropsType) {
+    const onClickButtonHandler =(event:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+        event.currentTarget.closest('li')?.remove()//my firsr decision for deleting
+    }
     return <div>
         <h3>{props.title}</h3>
         <div>
@@ -19,9 +22,18 @@ export function Todolist(props: PropsType) {
             <button>+</button>
         </div>
         <ul>
-            <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+            {props.tasks.map(el=>{
+                return (
+                    <li key={el.id}>
+                        <button onClick={onClickButtonHandler}>X</button>
+                        <input type="checkbox" checked={el.isDone} onChange={()=>{}}/>
+                        <span>{el.title}</span>
+                    </li>)
+                })
+            }
+            {/* <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
             <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-            <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li> */}
         </ul>
         <div>
             <button>All</button>
