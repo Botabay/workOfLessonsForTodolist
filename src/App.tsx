c7d2d8e97e37160398a3cf06b2b40a0b953d00ea
Botabay <tohgl49@gmail.com>
2023-04-14 22:14:01 +0600

    used useState for drawing a remote data, step3

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,44 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {Todolist} from './Todolist';
-import { v4 as uuid } from 'uuid'
+import { log } from 'console';
 
-function App() {
-    let a=11;
-    useEffect(()=>{
+type DataType={
+    id:number
+    title:string
+    completed:boolean
+}
+export const App = () => {
+    let [todosSt, setTodosSt] = useState<DataType[]>([])
+    // useEffect(() => {
+    //     fetch('https://jsonplaceholder.typicode.com/todos')
+    //         .then(response => response.json())
+    //         .then(json => setTodosSt(json))
+    // }, [todosSt])
+    const showTodos=()=>{
         fetch('https://jsonplaceholder.typicode.com/todos')
             .then(response => response.json())
-            .then(json => console.log(json))
-      },[a])
+            .then(json => setTodosSt(json))
+    }
+    const hideTodos=()=>{
+        setTodosSt([])
+    }
     return (
         <div className="App">
-            
+            <button onClick={showTodos}>show me Todos</button>
+            <button onClick={hideTodos}>hide me Todos</button>
+            <ul>
+                {todosSt.map((el, ind) => {
+                    return (
+                        <li key={ind}>
+                            <span>{el.id}</span>
+                            <span>{el.title}</span>
+                            <span>{el.completed}</span>
+                        </li>
+                    )
+                }
+                )}
+            </ul>
+
         </div>
     );
 }
-
-export default App;
-
-
-//-------------------------------------------------------------------------
-
-// import React, {useState} from 'react';
-// import './App.css';
-// import {Todolist} from './Todolist';
-//
-//
-// export type FilterValuesType = "all" | "active" | "completed" | "three";
-//
-// function App() {
-//
-//     let [tasks, setTasks] = useState([
-//         {id: 1, title: "HTML&CSS", isDone: true},
-//         {id: 2, title: "JS", isDone: true},
-//         {id: 3, title: "ReactJS", isDone: false},
-//         {id: 4, title: "Rest API", isDone: false},
-//         {id: 5, title: "GraphQL", isDone: false},
-//     ]);
-//
-//     const deleteAllTasks = () => {
-//         setTasks([])
-//     }
-//
-//     function removeTask(id: number) {
-//         let filteredTasks = tasks.filter(t => t.id != id);
-//         setTasks(filteredTasks);
-//     }
-//
-//     // let [filter, setFilter] = useState<FilterValuesType>("all");
-//     //
-//     // let tasksForTodolist = tasks;
-//     //
-//     // if (filter === "active") {
-//     //     tasksForTodolist = tasks.filter(t => t.isDone === false);
-//     // }
-//     // if (filter === "completed") {
-//     //     tasksForTodolist = tasks.filter(t => t.isDone === true);
-//     // }
-//     //
-//     // function changeFilter(value: FilterValuesType) {
-//     //     setFilter(value);
-//     // }
-//
-//     return (
-//         <div className="App">
-//             <Todolist
-//                 title="What to learn"
-//                 tasks={tasks}
-//                 removeTask={removeTask}
-//                 //changeFilter={changeFilter}
-//                 deleteAllTasks={deleteAllTasks}
-//
-//             />
-//         </div>
-//     );
-// }
-//
-// export default App;
